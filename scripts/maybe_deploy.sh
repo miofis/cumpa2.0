7bea1536798e82f4bc0f68cf3b9b1b50ac42c6af
Ignacio Gutierrez <gutierrez_nacho@hotmail.com>
2019-09-24 00:17:14 -0300

    [review] firebase envs

--- scripts/maybe_deploy.sh
+++ scripts/maybe_deploy.sh
@@ -4,10 +4,10 @@ COMMIT_MESSAGE=$(git log -n 1 --pretty=format:%s $CIRCLE_SHA1 | cut -c1-8)
 if [ $CIRCLE_BRANCH = "master" ]; then
   # deploy to staging
   HEROKU_APP='cumpa2-0-staging' yarn heroku-deploy
-  API='https://cumpa2-0-staging.herokuapp.com' yarn firebase-deploy
+  API='https://cumpa2-0-staging.herokuapp.com' FIREBASE_ENV='staging' yarn firebase-deploy
   exit 0
 elif [ $COMMIT_MESSAGE = "[review]" ]; then
   # deploy to review
   HEROKU_APP='cumpa2-0-review' yarn heroku-deploy
-  API='https://cumpa2-0-review.herokuapp.com' yarn firebase-deploy
+  API='https://cumpa2-0-review.herokuapp.com' FIREBASE_ENV='review' yarn firebase-deploy
 fi
