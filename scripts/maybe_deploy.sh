693081fe9af398a7d0c13a16821a136c79b1419a
Ignacio Gutierrez <gutierrez_nacho@hotmail.com>
2019-09-24 00:42:30 -0300

    [review] enable heroku again

--- scripts/maybe_deploy.sh
+++ scripts/maybe_deploy.sh
@@ -10,6 +10,6 @@ if [ $CIRCLE_BRANCH = "master" ]; then
   exit 0
 elif [ $COMMIT_MESSAGE = "[review]" ]; then
   # deploy to review
-  # HEROKU_APP='cumpa2-0-review' yarn heroku-deploy
+  HEROKU_APP='cumpa2-0-review' yarn heroku-deploy
   API='https://cumpa2-0-review.herokuapp.com' FIREBASE_ENV='review' yarn firebase-deploy
 fi
