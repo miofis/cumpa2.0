d31b8e5f3a4b60e294170332474dfed1dd7aac26
Ignacio <gutierrez_nacho@hotmail.com>
2019-09-23 22:19:49 -0300

    Ci improvements (#6)

---
+++ scripts/maybe_deploy.sh
@@ -0,0 +1,13 @@
+# get first 8 characters of commit message (to compate with '[review]' string)
+COMMIT_MESSAGE=$(git log -n 1 --pretty=format:%s $CIRCLE_SHA1 | cut -c1-8)
+
+if [ $CIRCLE_BRANCH = "master" ]; then
+  # deploy to staging
+  HEROKU_APP=cumpa2-0-staging yarn heroku-deploy
+  # TODO: deploy frontend
+  exit 0
+elif [ $COMMIT_MESSAGE = "[review]" ]; then
+  # deploy to review
+  HEROKU_APP=cumpa2-0-review yarn heroku-deploy
+  # TODO: deploy frontend
+fi
